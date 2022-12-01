d5245ffe0a669d5aeead257faa58ba7637499198
praveen2315 <praveenmathew2315@gmail.com>
2022-12-01 11:36:09 +0530

    add user_id to article with  association of user and article

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_30_123133) do
+ActiveRecord::Schema.define(version: 2022_12_01_053459) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
