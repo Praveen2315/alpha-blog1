b33e6b2f5380acdba85031adac3ca54e22f29098
praveen2315 <praveenmathew2315@gmail.com>
2022-12-05 12:09:51 +0530

    update the set_user for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update]
     def show
-        @user = User.find(params[:id])
         @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
     def index
@@ -13,11 +13,9 @@ class UsersController < ApplicationController
     end
    
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully updated"
       redirect_to @user
@@ -28,6 +26,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id  
             flash[:notice] = "#{@user.username} Signup successfully."
             redirect_to articles_path
         else
@@ -36,8 +35,12 @@ class UsersController < ApplicationController
     end
 
     private
-
     def user_params
         params.require(:user).permit(:username, :email, :password)
     end
+
+    def set_user
+        @user = User.find(params[:id])
+    end
+
 end
