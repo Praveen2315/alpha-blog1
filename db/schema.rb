67d38d8f114a483e78d49f8ffe89b6aa8c82db62
praveen2315 <praveenmathew2315@gmail.com>
2022-12-12 16:26:10 +0530

    add mamny to many association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_12_073431) do
+ActiveRecord::Schema.define(version: 2022_12_12_104841) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
